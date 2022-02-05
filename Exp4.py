0de639e297a87649d1c5a24612b84f276746c468
Bhagwatiahirwar23 <98872206+Bhagwatiahirwar23@users.noreply.github.com>
2022-02-05 21:23:59 +0530

    Add files via upload

---
+++ Exp4.py
@@ -0,0 +1,14 @@
+#Q4.Implement the insertion sort algorithm to sort given set of integer values in ascending order.
+def InsertionSort(Array):
+    for index in range (1,len(Array)):
+        current_element = Array[index]
+        position = index
+        while current_element < Array[position-1] and position > 0:
+            Array[position] = Array[position-1]
+            position = position-1
+        Array[position] = current_element
+
+Array = [4,7,9,2,1]
+InsertionSort(Array)      # FUNCTION CALL
+print(Array)
+
